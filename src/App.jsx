78d957060a614b098b108269202c2cd91acb5b1a
Diana Carolina Hernandez <dianacaro3552@gmail.com>
2023-05-11 17:43:50 -0500

    First commit, starting to modify the base

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+  let name ='Caro'
+  let persona = {
+    nombre: 'Carolina',
+    edad: 31
+  }
+
+  return (
+    <>
+      <div className='w-full h-screen flex flex-col items-center justify-center'>
+        <a className='bg-purple-400 p-16' href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+        <h1>Vite + React</h1>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+          <p>
+            {name} - {persona.edad}
+          </p>
+          <p>
+            Edit <code>src/App.jsx</code> and save to test HMR
+          </p>
+        </div>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
