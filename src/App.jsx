266f41852cf88c2ebfbf1e38837dd16043276a5f
Diana Carolina Hernandez <dianacaro3552@gmail.com>
2023-05-22 12:23:49 -0500

    Advance with carousel

--- src/App.jsx
+++ src/App.jsx
@@ -41,16 +41,20 @@ function App() {
         </div>
 
         {/* Carousel section */}
-        <div className='hidden md:flex justify-center my-10 mx-14'>
-          <div className='flex justify-between text-white px-20 py-16 bg-gradient-to-r from-indigo-700 to-[#5E52F3]'>
-            <p>imagen 1</p>
-            <p>imagen 2</p>
-            <div className='w-6/12'>
+        <div className='hidden md:flex justify-center m-14'>
+          <div className='flex justify-between text-white py-1 px-14 bg-gradient-to-r from-indigo-700 to-[#5E52F3]'>
+
+            <img className='w-1/5 h-auto -mt-8 object-cover' src="../../src/assets/carousel-img1.png" alt="imagen 1" />
+
+            <img className='w-1/5 -mt-10 mb-8 overflow-y-visible object-top' src="../../src/assets/carousel-img2.png" alt="imagen 2" />
+
+            <div className='w-6/12 flex flex-col justify-center'>
               <h3 className='text-xl font-medium'>Shonen:</h3>
               <p className='text-xs'>Is the manga that is aimed at adolescent boys. They are series with
                 large amounts of action, in which humorous situations often occur.
                 The camaraderie between members of a collective or a combat team stands out.</p>
             </div>
+
           </div>
         </div>
 
