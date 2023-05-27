c0996b8fda65aed95f0203890f0f447bbe1969f5
Diana Carolina Hernandez Abahonza <dianacaro3552@gmail.com>
2023-05-26 20:32:55 -0500

    Minga 01 (#2)
    * Cleaning past template
    
    * fix hamburger icon
    
    * Content for main section without background
    
    * Structure for footer
    
    * Structure for carousel section
    
    * Starting with images
    
    * Use of background images from tailwind config file
    
    * Advance with carousel
    
    * Updating footer
    
    * Add svg curve to image in footer
    
    * Updating footer
    
    * Fixing hero section spacing

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ module.exports = {
       './src/**/*.{js,jsx,ts,tsx}'
   ],
   theme: {
-      extend: {}
+      fontFamily: {
+        roboto: ['Roboto', 'sans-serif'],
+        roboto_serif: ['Roboto'],
+        poppins: ['Poppins'],
+      },
+      extend: {
+        backgroundImage: {
+          'hero-mobile': "url('../../src/assets/home_mobile.png')",
+          'hero-desktop': "url('../../src/assets/home_desktop.png')",
+          'footer_db': "url('../../src/assets/footer_db.png')",
+        }
+      }
   },
   variants: {},
   plugins: []
