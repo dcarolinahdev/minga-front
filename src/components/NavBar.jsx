f95d548351c0bf90b04fa5aff13d13f599c2f2e9
Diana Carolina Hernandez Abahonza <dianacaro3552@gmail.com>
2023-06-06 19:07:33 -0500

    States for navbar: navigation options (#9)

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,20 +1,33 @@
 import logo_manga from '/assets/logo_manga.png'
+import Display from './Display'
+import { useState } from 'react'
 
 export default function NavBar () {
+    let options = [
+        {title: 'Home'},
+        {title: 'Mangas'},
+        {title: 'My Mangas'},
+        {title: 'Favorites'},
+        {title: 'Logout'}
+    ]
+    let [show, setShow] = useState(false)
     return (
-        <nav className='w-full absolute flex justify-between items-center p-2'>
-            {/* Hamburger menu button */}
-            <svg
-                className="w-8 h-8 fill-current text-indigo-700 md:text-white"
-                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                strokeWidth="2" stroke="currentColor">
-                <path
-                    strokeLinecap="round" strokeLinejoin="round"
-                    d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"/>
-            </svg>
+        <>
+            {show && <Display options={options} />}
+            <nav className='w-full absolute flex justify-between items-center p-2'>
+                {/* Hamburger menu button */}
+                <svg
+                    className="w-8 h-8 fill-current text-indigo-700 md:text-white"
+                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                    strokeWidth="2" stroke="currentColor" onClick={()=>setShow(!show)}>
+                    <path
+                        strokeLinecap="round" strokeLinejoin="round"
+                        d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"/>
+                </svg>
 
-            {/* Logo */}
-            <img src={logo_manga} alt="Logo" />
-        </nav>
+                {/* Logo */}
+                <img className='z-10' src={logo_manga} alt="Logo" />
+            </nav>
+        </>
     )
 }
