7034470790933667226bdfffdbcefb9480990a76
Diana Carolina Hernandez Abahonza <dianacaro3552@gmail.com>
2023-06-17 11:29:59 -0500

    fixing background hamburger icon

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -16,14 +16,16 @@ export default function NavBar () {
             {show && <Display options={options} show={show} setShow={setShow} />}
             <nav className='w-full absolute flex justify-between items-center p-[2.25%]'>
                 {/* Hamburger menu button */}
-                <svg
-                    className="w-8 h-8 fill-current text-indigo-700 md:text-white"
-                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                    strokeWidth="2" stroke="currentColor" onClick={()=>setShow(!show)}>
-                    <path
-                        strokeLinecap="round" strokeLinejoin="round"
-                        d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"/>
-                </svg>
+                <div className='bg-indigo-700 p-1 rounded-md'>
+                    <svg
+                        className="w-8 h-8 fill-current text-indigo-700 md:text-white"
+                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                        strokeWidth="2" stroke="currentColor" onClick={()=>setShow(!show)}>
+                        <path
+                            strokeLinecap="round" strokeLinejoin="round"
+                            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"/>
+                    </svg>
+                </div>
 
                 {/* Logo */}
                 <img className='z-10' src={logo_manga} alt="Logo" />
