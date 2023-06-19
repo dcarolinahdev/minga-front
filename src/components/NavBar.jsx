462a359d5f1b05ed99381f75c06e80b8095ddcb1
Diana Carolina Hernandez Abahonza <dianacaro3552@gmail.com>
2023-06-19 16:07:34 -0500

    Update spacing and sizes in Navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -15,9 +15,13 @@ export default function NavBar () {
             {show && <Display options={options} show={show} setShow={setShow} />}
             <nav className='w-full absolute flex justify-between items-center p-[2.25%]'>
                 {/* Hamburger menu button */}
-                <div className='h-8 sm:h-10 md:h-12 lg:h-14 flex items-center px-2 bg-indigo-700 hover:bg-indigo-600 rounded-md cursor-pointer'>
+                <div
+                    className='h-6 sm:h-8 md:h-10 lg:h-10 2xl:h-12
+                               w-8 sm:w-10 md:w-12 lg:w-12 2xl:w-14
+                               flex justify-center items-center cursor-pointer
+                               bg-indigo-700 hover:bg-indigo-600 rounded-md'>
                     <svg
-                        className="w-8 h-8 fill-current text-white"
+                        className="w-6 md:w-8 2xl:w-10 px-[2px] fill-current text-white"
                         xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                         strokeWidth="2" stroke="currentColor" onClick={()=>setShow(!show)}>
                         <path
@@ -27,7 +31,7 @@ export default function NavBar () {
                 </div>
 
                 {/* Logo */}
-                <img className='h-8 sm:h-10 md:h-12 lg:h-14' src='/assets/Logo_Blue.png' alt="Logo" />
+                <img className='h-6 sm:h-8 md:h-10 lg:h-10 2xl:h-12' src='/assets/Logo_Blue.png' alt="Logo" />
             </nav>
         </>
     )
