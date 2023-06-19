eb03bd2798668e45216aefbe687233156bc9cf04
Diana Carolina Hernandez Abahonza <dianacaro3552@gmail.com>
2023-06-19 16:06:31 -0500

    Update spacing in button hero

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,9 @@
 export default function Button() {
   return (
-    <button className='text-2xl font-bold rounded-xl py-4 px-32 bg-indigo-700 hover:bg-indigo-600 md:bg-gradient-to-r from-indigo-700 to-[#5E52F3]' >
+    <button
+      className='text-md sm:text-lg md:text-xl lg:text-2xl font-bold
+                 w-full rounded-xl bg-indigo-700 hover:bg-indigo-600 lg:bg-gradient-to-r from-indigo-700 to-[#5E52F3]
+                 py-2 xl:py-3'>
         Started
     </button>
   )
